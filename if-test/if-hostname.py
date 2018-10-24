bf2de6360750086be71be6b4706f11ad20c01074
d3239939 <d3239939@gmail.com>
2018-10-24 15:57:01 -0400

    testing with if

---
+++ if-test/if-hostname.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+#hostname = 'MTG'
+
+hostname = input("What is your hostname? ")
+hostname = hostname.upper()
+
+if hostname == 'MTG':
+  print('The hostname was found to be MTG.')
+  print('The hostname matches expected config.')
+elif hostname != 'MTG':
+  print('The hostname does not match the expected config.')
+
+print('Exiting the script...')
+
+#Saving time by not commenting.
+
